656ee66d0c472289390624047a5b42df609e8ede
mikaelagurney <mikaelagurney@gmail.com>
2020-06-17 00:06:47 -0700

    Remove extra files

--- src/pages/links.js
+++ src/pages/links.js
@@ -1,11 +1,6 @@
 import React from "react"
 import Layout from "../components/Layout.js"
-import PixelAnimation from "../components/PixelAnimation.js"
 
 export default function Links() {
-  return (
-    <Layout>
-      <PixelAnimation></PixelAnimation>
-    </Layout>
-  )
+  return <Layout>Under Construction</Layout>
 }
