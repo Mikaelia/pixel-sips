d6b05a6447cee7616b24f26df7f52812861e0e9d
mikaelagurney <mikaelagurney@gmail.com>
2020-06-25 11:32:14 -0700

    Add conditional mobile animation rendering

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 import { useSpring, animated } from "react-spring"
+import useWindowSize from "../utils/useWindowSize"
 
 const calc = (x, y) => [
   -(y - window.innerHeight / 2) / 40,
@@ -21,6 +22,38 @@ const StyledPost = styled(animated.div)`
   border-color: rgb(228, 228, 231);
   border-image: initial;
 
+  :hover {
+    box-shadow: rgba(73, 73, 80, 0.4) 2px 4.45528px 6.06029px,
+      rgba(73, 73, 80, 0.05) 0px 10.7067px 14.5637px,
+      rgba(73, 73, 80, 0.04) 0px 20.1597px 27.4222px,
+      rgba(73, 73, 80, 0.03) 0px 35.9615px 48.9165px,
+      rgba(73, 73, 80, 0.02) 0px 67.2619px 91.493px,
+      rgba(73, 73, 80, 0.01) 0px 161px 219px;
+    border-radius: 8px;
+  }
+`
+
+const StyledPostMobile = styled.div`
+  padding: 3rem;
+  border-radius: 0.8rem;
+  transition: 0.35s ease-out;
+  background: #fff;
+  border-width: 1px;
+  border-style: solid;
+  border-color: rgb(228, 228, 231);
+  border-image: initial;
+
+  :hover {
+    box-shadow: rgba(73, 73, 80, 0.4) 2px 4.45528px 6.06029px,
+      rgba(73, 73, 80, 0.05) 0px 10.7067px 14.5637px,
+      rgba(73, 73, 80, 0.04) 0px 20.1597px 27.4222px,
+      rgba(73, 73, 80, 0.03) 0px 35.9615px 48.9165px,
+      rgba(73, 73, 80, 0.02) 0px 67.2619px 91.493px,
+      rgba(73, 73, 80, 0.01) 0px 161px 219px;
+    border-radius: 8px;
+  }
+`
+const StyledArticle = styled.article`
   article {
     height: 100%;
   }
@@ -43,14 +76,6 @@ const StyledPost = styled(animated.div)`
   }
 
   :hover {
-    box-shadow: rgba(73, 73, 80, 0.4) 2px 4.45528px 6.06029px,
-      rgba(73, 73, 80, 0.05) 0px 10.7067px 14.5637px,
-      rgba(73, 73, 80, 0.04) 0px 20.1597px 27.4222px,
-      rgba(73, 73, 80, 0.03) 0px 35.9615px 48.9165px,
-      rgba(73, 73, 80, 0.02) 0px 67.2619px 91.493px,
-      rgba(73, 73, 80, 0.01) 0px 161px 219px;
-    border-radius: 8px;
-
     h3 {
       color: ${props => props.theme.pink};
     }
@@ -79,27 +104,49 @@ export default function Post({ node }) {
     config: { mass: 5, tension: 350, friction: 40 },
   }))
   const title = node.frontmatter.title || node.fields.slug
+  let windowSize = useWindowSize()
+
   return (
-    <StyledPost
-      style={{ transform: props.xys.interpolate(trans) }}
-      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-      onMouseLeave={() => set({ xys: [0, 0, 1] })}
-    >
-      <article key={node.fields.slug}>
-        <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-          <header>
-            <h3>{title}</h3>
-          </header>
-          <section>
-            <p
-              dangerouslySetInnerHTML={{
-                __html: node.frontmatter.description || node.excerpt,
-              }}
-            />
-          </section>
-          <div className="read-more">Read more</div>
-        </Link>
-      </article>
-    </StyledPost>
+    (windowSize.width > 768 && (
+      <StyledPost
+        style={{ transform: props.xys.interpolate(trans) }}
+        onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+        onMouseLeave={() => set({ xys: [0, 0, 1] })}
+      >
+        <StyledArticle key={node.fields.slug}>
+          <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+            <header>
+              <h3>{title}</h3>
+            </header>
+            <section>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt,
+                }}
+              />
+            </section>
+            <div className="read-more">Read more</div>
+          </Link>
+        </StyledArticle>
+      </StyledPost>
+    )) || (
+      <StyledPostMobile>
+        <StyledArticle key={node.fields.slug}>
+          <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+            <header>
+              <h3>{title}</h3>
+            </header>
+            <section>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt,
+                }}
+              />
+            </section>
+            <div className="read-more">Read more</div>
+          </Link>
+        </StyledArticle>
+      </StyledPostMobile>
+    )
   )
 }
